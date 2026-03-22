f9b31d63d4f0d69a9cdd6f66f03c9f9cf0a1bc94
MutantCacti <mutantcacti@gmail.com>
2026-03-22 18:00:16 +0000

    Add Audio MDX component and Halting reading

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import YouTube from './YouTube'
+import Audio from './Audio'
 import Credit from './Credit'
 import Poem from './Poem'
 
 export const mdxComponents = {
     YouTube,
+    Audio,
     Credit,
     Poem,
     a: (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
